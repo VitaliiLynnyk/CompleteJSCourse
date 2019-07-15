118033ff71065e2284bbcdb6bc1a683ca726d8bf
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-07-15 14:39:35 +0300

    Added qoute generators on page onload

---
+++ index.js
@@ -0,0 +1,18 @@
+const URL = "https://favqs.com/api/qotd";
+
+function fetchQoute(url) {
+  fetch(url).then(response => {
+    return response.json();
+  }).then(data => {
+    const qouteElement = document.querySelector(".qoute_containter");
+    createParagraph(data.quote.body, qouteElement);
+  });
+}
+
+function createParagraph(text, appendTo) {
+  const p = document.createElement("p");
+  p.innerHTML = text;
+  appendTo.appendChild(p);
+}
+
+window.addEventListener("onload", fetchQoute(URL));

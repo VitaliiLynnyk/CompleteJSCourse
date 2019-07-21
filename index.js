d3665ac03cbb382ab794ade03bd5888c04caa9ae
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-07-21 12:14:27 +0300

    Added challenge4 && media to pre font-size

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const URL = "https://favqs.com/api/qotd";
+const URL = 'https://favqs.com/api/qotd';
 
 function fetchQoute(url) {
-  fetch(url).then(response => {
-    return response.json();
-  }).then(data => {
-    const qouteElement = document.querySelector(".qoute_containter");
-    createParagraph(data.quote.body, qouteElement);
-  });
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const qouteElement = document.querySelector('.qoute_containter');
+      createParagraph(data.quote.body, qouteElement);
+    });
 }
 
 function createParagraph(text, appendTo) {
-  const p = document.createElement("p");
+  const p = document.createElement('p');
   p.innerHTML = text;
   appendTo.appendChild(p);
 }
 
-window.addEventListener("onload", fetchQoute(URL));
+window.addEventListener('onload', fetchQoute(URL));

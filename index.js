96e634ccf6ec64f9583ddcc94f123cabb70dac55
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-07-30 18:24:35 +0300

    Changed prettier setting-4 spaces, removed css in 6 chellange

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const URL = 'https://favqs.com/api/qotd';
 
 function fetchQoute(url) {
-  fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      const qouteElement = document.querySelector('.qoute_containter');
-      createParagraph(data.quote.body, qouteElement);
+    fetch(url)
+        .then(response => {
+            return response.json();
+        }).then(data => {
+        const qouteElement = document.querySelector('.qoute_containter');
+        createParagraph(data.quote.body, qouteElement);
     });
 }
 
-function createParagraph(text, appendTo) {
-  const p = document.createElement('p');
-  p.innerHTML = text;
-  appendTo.appendChild(p);
+function createParagraph(text, appendTo, htmlParagraphElement = document.createElement("p"), htmlParagraphElement1 = htmlParagraphElement, ixmldomNode = appendTo.appendChild(p), ixmldomNode1 = ixmldomNode) {
+    const p = htmlParagraphElement1;
+    p.innerHTML = text;
+    ixmldomNode1;
 }
 
 window.addEventListener('onload', fetchQoute(URL));

942809ce5afd3dc4c0a250e36d3c1837a0e28b7e
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-07-31 09:13:37 +0300

    Fixed qoutes error && removed prettier autosave

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const URL = 'https://favqs.com/api/qotd';
+const URL = "https://favqs.com/api/qotd";
 
 function fetchQoute(url) {
-    fetch(url)
-        .then(response => {
-            return response.json();
-        }).then(data => {
-        const qouteElement = document.querySelector('.qoute_containter');
-        createParagraph(data.quote.body, qouteElement);
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const qouteElement = document.querySelector(".qoute_containter");
+      createParagraph(data.quote.body, qouteElement);
     });
 }
 
-function createParagraph(text, appendTo, htmlParagraphElement = document.createElement("p"), htmlParagraphElement1 = htmlParagraphElement, ixmldomNode = appendTo.appendChild(p), ixmldomNode1 = ixmldomNode) {
-    const p = htmlParagraphElement1;
-    p.innerHTML = text;
-    ixmldomNode1;
+function createParagraph(text, parentElement) {
+  const p = document.createElement("p");
+  p.innerHTML = text;
+  parentElement.appendChild(p);
 }
 
-window.addEventListener('onload', fetchQoute(URL));
+window.addEventListener("onload", fetchQoute(URL));

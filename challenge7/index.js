9fbfcf7ca0a44e07f17a95f1681af8c619879cd5
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-08-09 22:55:53 +0300

    Added test random generation questions, answers

--- challenge7/index.js
+++ challenge7/index.js
@@ -11,3 +11,13 @@ class Answer {
     this.correct = correct;
   }
 }
+
+const questionsText = ["What is your favorite store in the mall?",
+                    "What's the longest you've gone without showering?",
+                    "If you found $100 on the ground, what would you do with it?"];
+
+const answersText = [ "Yes", "Nope", "Maybe"];
+
+const answersArray = answersText.map(item => new Answer(item, Math.round(Math.random())));
+
+const questionArray = new Array(3).fill(0).map(item => new Question(questionsText[Math.floor(Math.random()*questionsText.length)], answersArray));

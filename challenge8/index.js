6553fe34e5d2e66ead4e1da7f712c3c2879e83a7
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-08-15 15:32:28 +0300

    Added expense, income constructor, init func

--- challenge8/index.js
+++ challenge8/index.js
@@ -1,11 +1,29 @@
 const budgetController = (() => {
-  
+
+  const Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
 })();
 
 const UIController = (() => {
-  
+
 })();
 
 const controller = ((budgetCtrl, UICtrl) => {
-  
+  return {
+    init: function () {
+      console.log('Application has started.');
+    }
+  };
 })(budgetController, UIController);
+
+controller.init();
